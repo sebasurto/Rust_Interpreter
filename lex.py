801aed7bef4caa79cf43cf576bf3a2dd3cbfdfbf
Ramón Macías <rimacias@espol.edu.ec>
2023-06-12 23:54:23 -0500

    Tokens
    Correccion de ciertos tokens y adición de algunos de ellos para acercarnos al léxico de Rust. Se añadió la función para el test de Ramón Macías

--- lex.py
+++ lex.py
@@ -1,80 +1,152 @@
 import ply.lex as lex
-
-reserved = {'while': 'WHILE', 'where':'WHERE','use':'USE','unsafe':'UNSAFE', 'union':'UNION','type':'TYPE',
-            'true':'TRUE','trair':'TRAIT','super':'SUPER','struct':'STRUCT','static':'STATIC','self':'SELF',
-            'Self':'sELF','return':'RETURN','ref':'REF','pub':'PUB','mut':'MUT','move':'MOVE','mod':'MOD','match':'MATCH',
-            'loop':'LOOP','let':'LET','in':'IN','impl':'IMPL', 'if':'IF','for':'FOR','fn':'FN','false':'FALSE',
-            'extern':'EXTERN','enum':'ENUM', 'else':'ELSE', 'dyn':'DYN','crate':'CRATE', 'continue':'CONTINUE',
-            'const':'CONST', 'break': 'BREAK', 'await':'AWAIT', 'async':'ASYNC', 'as':'AS'
+#Ramon Macias y Sergio Basurto
+reserved = {
+    'while': 'WHILE',
+    'where': 'WHERE',
+    'use': 'USE',
+    'unsafe': 'UNSAFE',
+    'union': 'UNION',
+    'type': 'TYPE',
+    'true': 'TRUE',
+    'trair': 'TRAIT',
+    'super': 'SUPER',
+    'struct': 'STRUCT',
+    'static': 'STATIC',
+    'self': 'SELF',
+    'Self': 'SELF',
+    'return': 'RETURN',
+    'ref': 'REF',
+    'pub': 'PUB',
+    'mut': 'MUT',
+    'move': 'MOVE',
+    'mod': 'MOD',
+    'match': 'MATCH',
+    'loop': 'LOOP',
+    'let': 'LET',
+    'in': 'IN',
+    'impl': 'IMPL',
+    'if': 'IF',
+    'for': 'FOR',
+    'fn': 'FN',
+    'false': 'FALSE',
+    'extern': 'EXTERN',
+    'enum': 'ENUM',
+    'else': 'ELSE',
+    'dyn': 'DYN',
+    'crate': 'CRATE',
+    'continue': 'CONTINUE',
+    'const': 'CONST',
+    'break': 'BREAK',
+    'await': 'AWAIT',
+    'async': 'ASYNC',
+    'as': 'AS'
 }
 
- #Sequencia de tokens, puede ser lista o tupla
+
+# Sequencia de tokens, puede ser lista o tupla
+# Ramon Macias y Sergio Basurto (Ambos trabajamos en esta parte)
 tokens = (
-    'COMMENS',
-    'EQUALSC',
-    'EQUALS',
-    'FLOAT',
-    'INT',
-    #'NUMBER',
     'PLUS',
     'MINUS',
     'TIMES',
     'DIVIDE',
     'LPAREN',
     'RPAREN',
-    'ID',
+    'LBRACKET',
+    'RBRACKET',
+    'LBRACE',
+    'RBRACE',
+    'SEMICOLON',
+    'COMMA',
+    'DOT',
+    'COLON',
+    'DOUBLE_COLON',
+    'ARROW',
+    'FAT_ARROW',
+    'PIPE',
+    'PIPE_PIPE',
+    'AMPERSAND',
+    'AMPERSAND_AMPERSAND',
+    'BANG',
+    'EQUAL',
+    'EQUAL_EQUAL',
     'LESS_THAN',
     'GREATER_THAN',
     'LESS_THAN_EQUAL',
     'GREATER_THAN_EQUAL',
-    'CHAR',
+    'NOT_EQUAL',
+    'MODULO',
+    'UNDERSCORE',
+    'AT',
 ) + tuple(reserved.values())
- 
- #Exp Regulares para tokens de símbolos
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_TIMES   = r'\*'
-t_DIVIDE  = r'/'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
-#t_NUMBER = r'\d+'
-t_INT = r'\d+'
-t_FLOAT = r'-?\d*\.\d+'
-t_EQUALS = '='
-t_EQUALSC = '=='
+
+# Expresiones regulares para los tokens de símbolos
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
+t_SEMICOLON = r';'
+t_COMMA = r','
+t_DOT = r'\.'
+t_COLON = r':'
+t_DOUBLE_COLON = r'::'
+t_ARROW = r'->'
+t_FAT_ARROW = r'=>'
+t_PIPE = r'\|'
+t_PIPE_PIPE = r'\|\|'
+t_AMPERSAND = r'&'
+t_AMPERSAND_AMPERSAND = r'&&'
+t_BANG = r'!'
+t_EQUAL = r'='
+t_EQUAL_EQUAL = r'=='
 t_LESS_THAN = r'<'
 t_GREATER_THAN = r'>'
 t_LESS_THAN_EQUAL = r'<='
 t_GREATER_THAN_EQUAL = r'>='
-t_CHAR = r'\'[a-zA-Z]\''
- #Para contabilizar nro de líneas
+t_NOT_EQUAL = r'!='
+t_MODULO = r'%'
+t_UNDERSCORE = r'_'
+t_AT = r'@'
+
+
+# Para contabilizar nro de líneas
 def t_newline(t):
-  r'\n+'
-  t.lexer.lineno += len(t.value)
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
 
 def t_ID(t):
-  r'[a-zA-Z_]\w+'
-  t.type = reserved.get(t.value,'ID')
-  return t
+    r'[a-zA-Z_]\w*'
+    t.type = reserved.get(t.value.lower(), 'ID')
+    return t
 
- 
  # Ignorar lo que no sea un token en mi LP
-t_ignore  = ' \t'
- 
- #Presentación de errores léxicos
+t_ignore = ' \t'
+
+# Presentación de errores léxicos
+
+
 def t_error(t):
-  print("Componente léxico no reconocido '%s'" % t.value[0])
-  t.lexer.skip(1)
- 
- #Contruir analizador
+    print("Componente léxico no reconocido '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+ # Contruir analizador
+
 
 def _COMMENS(t):
-  r'//.*'
-  pass
+    r'//.*'
+    pass
+
 
 def test_Sergio_Basurto():
-  lexer = lex.lex()
-  data = ''' 5 < 6
+    lexer = lex.lex()
+    data = ''' 5 < 6
             8 > 10
             7 <= 9
             8 >= 9
@@ -82,9 +154,26 @@ def test_Sergio_Basurto():
             //comentario
 
         '''
-  lexer.input (data)
-  while True:
-    tok = lexer.token()
-    if not tok: 
-      break      #Rompe
-    print(tok)
+    lexer.input(data)
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break  # Rompe
+        print(tok)
+
+
+def test_Ramon_Macias():
+    lexer = lex.lex()
+    data = ''' 2==1
+            2!=1
+            2<1
+            2>1
+            2<=1
+            2>=1
+            let x=51
+            // let x=1
+            y!=z
+            '''
+    lexer.input(data)
+    while tok := lexer.token():
+        print(tok)
--- main.py
+++ main.py
@@ -2,4 +2,5 @@ import interface as fc
 import lex
 
 #fc.create_interface ()
-lex.test_Sergio_Basurto()
+#lex.test_Sergio_Basurto()
+lex.test_Ramon_Macias()
